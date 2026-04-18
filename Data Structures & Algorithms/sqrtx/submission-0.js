c037527896cf250473190da0bcbfea5ab74fe091
Saurav Sharma <110561859+sauravsharma2025@users.noreply.github.com>
2026-04-18 10:51:12 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    mySqrt(x) {
+        if(x<2)return x
+        let left=0
+        let ans=0
+        let right=Math.floor((x/2))
+        while(left<=right){
+            const mid=Math.floor((left+right)/2)
+            if(mid*mid===x)return mid
+            if(mid*mid<x){
+                ans=mid
+                left=mid+1
+            }else{
+                right=mid-1
+            }
+            }
+            return ans
+    }
+}
